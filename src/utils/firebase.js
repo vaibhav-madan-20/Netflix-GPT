88ab5a19d4596ac8e19fdebaeeec3dd1bde47a02
vaibhav1565 <vaibhavmadan15@gmail.com>
2025-02-17 18:13:04 +0530

    Initial commit

---
+++ src/utils/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDKE5LrU2Z0h0uhvfzuzIvctc6sYMoj4cY",
+    authDomain: "vaibhav-netflix.firebaseapp.com",
+    projectId: "vaibhav-netflix",
+    storageBucket: "vaibhav-netflix.firebasestorage.app",
+    messagingSenderId: "649573915765",
+    appId: "1:649573915765:web:1b91c8cc6191899e4e6c3c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth();
